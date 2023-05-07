043cce85255b3efb760f808be44d221500511cf3
IAM-B <salamualyk@pm.me>
2023-05-07 16:25:46 +0300

    Adding the  modal window feature with sourceCode integration

--- projectVocArabic/sourceCode/Scripts/ux.js
+++ projectVocArabic/sourceCode/Scripts/ux.js
@@ -82,3 +82,25 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Sélectionner tous les éléments avec la classe "button"
+const buttons = document.querySelectorAll(".buttonModal");
+
+// Ajouter un écouteur d'événement click à chaque bouton
+buttons.forEach((modal) => {
+  modal.addEventListener("click", function () {
+    const buttonId = this.getAttribute("id");
+    const modalContainer = document.getElementById("modal-container");
+    modalContainer.removeAttribute("class");
+    modalContainer.classList.add(buttonId);
+    document.body.classList.add("modal-active");
+  });
+});
+
+// Sélectionner l'élément avec l'id "modal-container"
+const modalContainer = document.getElementById("modal-container");
+
+// Ajouter un écouteur d'événement click à l'élément modal-container
+modalContainer.addEventListener("click", function () {
+  this.classList.add("out");
+  document.body.classList.remove("modal-active");
+});
