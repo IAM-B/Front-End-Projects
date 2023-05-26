02646b9262af12682fd9556dad5f37512ac71991
IAM-B <salamualyk@pm.me>
2023-05-26 10:34:57 +0300

    Created HTML file for the incrementation systeme.io

--- IELAM/Assets/Scripts/asyncScript.js
+++ IELAM/Assets/Scripts/asyncScript.js
@@ -1,26 +1,27 @@
 const section = document.querySelector("section");
-const exerciceIds = [
-  "exercice-1",
-  "exercice-2",
-  "exercice-3"
-];
+const exerciceIds = ["exercice-1", "exercice-2", "exercice-3"];
 
 const scriptUrls = {
-  "exercice-1": "Tome1/tome1-vocab1.json",
-  "exercice-2": "Tome1/tome1-vocab2.json",
-  "exercice-3": "Tome1/tome1-vocab3.json"
+  "exercice-1":
+    "https://raw.githubusercontent.com/IAM-B/Frontend-Projects/main/IELAM/Tome1/tome1-vocab1.json",
+  "exercice-2":
+    "https://raw.githubusercontent.com/IAM-B/Frontend-Projects/main/IELAM/Tome1/tome1-vocab2.json",
+  "exercice-3":
+    "https://raw.githubusercontent.com/IAM-B/Frontend-Projects/main/IELAM/Tome1/tome1-vocab3.json",
 };
 
-let currentExerciceIndex = parseInt(localStorage.getItem("currentExerciceIndex"));
+let currentExerciceIndex = parseInt(
+  localStorage.getItem("currentExerciceIndex")
+);
 
 if (isNaN(currentExerciceIndex) || currentExerciceIndex >= exerciceIds.length) {
   currentExerciceIndex = 0;
 }
 
 // Charger le fichier JSON
-fetch('Tome1/tome1-vocab1.json')
-  .then(response => response.json())
-  .then(data => {
+fetch(scriptUrls[exerciceIds[currentExerciceIndex]])
+  .then((response) => response.json())
+  .then((data) => {
     // Stocker le tableau de vocabulaires dans la variable "vocabularies"
     const vocabularies = data;
 
@@ -37,9 +38,15 @@ fetch('Tome1/tome1-vocab1.json')
 
     section.id = exerciceIds[currentExerciceIndex];
     currentExerciceIndex = (currentExerciceIndex + 1) % exerciceIds.length;
-    localStorage.setItem("currentExerciceIndex", currentExerciceIndex.toString());
+    localStorage.setItem(
+      "currentExerciceIndex",
+      currentExerciceIndex.toString()
+    );
   })
-  .catch(error => {
+  .catch((error) => {
     // Gérer les erreurs lors du chargement du fichier JSON
-    console.error('Une erreur s\'est produite lors du chargement du fichier JSON:', error);
+    console.error(
+      "Une erreur s'est produite lors du chargement du fichier JSON:",
+      error
+    );
   });
