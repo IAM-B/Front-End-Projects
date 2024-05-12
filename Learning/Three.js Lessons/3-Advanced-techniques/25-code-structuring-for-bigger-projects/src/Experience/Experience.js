ec36ec813636e6d5fd5c04796a603ed40d4b3fec
IAM-B <salamualyk@pm.me>
2024-05-12 16:16:20 +0300

    Add code structuring for bigger projects lesson in Three.js Advanced techniques

---
+++ Learning/Three.js Lessons/3-Advanced-techniques/25-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,62 @@
+import * as THREE from 'three'
+import Sizes from "./Utils/Sizes.js"
+import Time from "./Utils/Time.js"
+import Camera from "./Camera.js"
+import Renderer from './Renderer.js'
+import World from './Wolrd/World.js'
+import sources from './sources.js'
+import Resources from './Utils/Resources.js'
+
+let instance = null
+
+export default class Experience 
+{
+    constructor(canvas)
+    {
+        if(instance)
+        {
+            return instance
+        }
+
+        instance = this
+
+        // Global access
+        window.experience = this
+
+        // Options
+        this.canvas = canvas
+
+        // Setup
+        this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.camera = new Camera()
+        this.resources = new Resources(sources)
+        this.renderer = new Renderer()
+        this.world = new World()
+
+        // Sizes resize event
+        this.sizes.on('resize', () => 
+        {
+            this.resize()
+        })
+        
+        // Time tick event
+        this.time.on('tick', () =>
+        {
+            this.update()
+        })
+    }
+
+    resize()
+    {
+        this.camera.resize()
+        this.renderer.resize()
+    }
+
+    update()
+    {
+        this.camera.update()
+        this.renderer.update()
+     }
+}
---
+++ Learning/Three.js Lessons/3-Advanced-techniques/25-code-structuring-for-bigger-projects/src/Experience/Wolrd/World.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three'
+import Experience from "../Experience.js";
+import Environment from './Environment.js';
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+         // Test mesh 
+         const testMesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshStandardMaterial()
+         )
+         this.scene.add(testMesh)
+
+         // Wait for resources
+         this.resources.on('ready', () =>
+         {
+            // Setup
+            this.environment = new Environment()
+         })
+
+
+    }
+}
